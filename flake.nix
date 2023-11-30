9ac9564e8ed8eff22f9d6363f93d3f187d6602f4
JP <dragonginger10@gmail.com>
2023-11-30 17:40:47 -0600

    set up first commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Flake for setting up my dev environment for Advent of code";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      goVersion = 21;
+      overlays = [ (self: super: { go = super."go_1_${toString goVersion}"; }) ];
+      pkgs = import nixpkgs { inherit overlays system; };
+      
+    in
+    {
+      formatter.default = pkgs.alejandra;
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          go
+          gotools
+          golangci-lint
+        ];
+
+        shellHook = ''
+          ${pkgs.go}/bin/go version
+          source ./secrets
+
+          function aoc-load () {
+            if [ $1 ]
+            then
+                curl --cookie "session=$AOC_COOKIE" https://adventofcode.com/$1/day/$1/input > in.txt
+            else
+                curl --cookie "session=$AOC_COOKIE" `date +https://adventofcode.com/%Y/day/%d/input` > in.txt
+            fi
+          }
+        '';
+      };
+    });
+}
