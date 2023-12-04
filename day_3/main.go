2cc4cfb220f99d7de8ba3aded31c14d12c40504f
JP <dragonginger10@gmail.com>
2023-12-04 14:45:25 -0600

    day three based on answers found scrolling through reddit pt.1

--- day_3/main.go
+++ day_3/main.go
@@ -1,25 +1,98 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+func ReadInput(file string) []string {
+  rawFile, err := os.ReadFile(file)
+  if err != nil {
+    log.Fatal(err)
+  }
+  return strings.Split(string(rawFile), "\n")
+}
+
+func getMatrix(line []string) [][]rune {
+  matrix := make([][]rune, len(line))
+
+  for i := 0; i < len(line); i++ {
+    matrix[i] = readChar(line[i])
+  }
+
+  return matrix
+}
+
+func readChar(line string) []rune {
+  var converter []rune
+  for _, runeValue := range line {
+    converter = append(converter, runeValue)
+  }
+  return converter
+}
+
+func getAdjacentSymbol(row int, start_col int, end_col int, matrix [][]rune) ([]int, error){
+  search_area := [][]int{{row, start_col-1}, {row, end_col}}
+
+  for i := row-1; i < row; i++ {
+    for j := start_col-1; j < end_col; j++ {
+      search_area = append(search_area, []int{i, j})
+    }
+  }
+
+  for _, coordinate := range search_area {
+    r := coordinate[0]
+    c := coordinate[1]
+
+    if r < 0 || r >= len(matrix) || c < 0 || c >= len(matrix[0]) {
+      continue
+    }
+
+    candidate := matrix[r][c]
+    if unicode.IsDigit(candidate) && candidate != '.' {
+      return []int{r, c}, nil
+    }
+  }
+  return nil, errors.New("WTF")
+}
+
 func main() {
-	file := os.Args[1]
+  var ans int
+  var partNum []int
+  grid := ReadInput(os.Args[1])
+  matrix := getMatrix(grid)
 
-	content, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
+  for _, i := range matrix {
+    i = append(i, '.')
+  }
 
-	defer content.Close()
+  for r,row := range matrix {
+    num := 0
+    for c, col := range row {
+      if unicode.IsDigit(col){
+        num *= 10
+        num += int(col)
+        continue
+      }
+      if num == 0 {
+        continue
+      }
+      col_start := c - len(string(num))
+      _, err := getAdjacentSymbol(r, col_start, c, matrix); if err != nil {
+        partNum = append(partNum, num)
+      }
+      num = 0
+    }
+  }
 
-	scanner := bufio.NewScanner(content)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-	}
+  for _, num := range partNum {
+    ans += num
+  }
+
+  fmt.Println(ans)
 }
