4c3fb256c3520aa6d854a13701f1ef8abd11463d
JP <dragonginger10@gmail.com>
2023-12-04 15:46:36 -0600

    working on test case
    solution ported from Andrew Negri

--- day_3/main.go
+++ day_3/main.go
@@ -38,23 +38,23 @@ func readChar(line string) []rune {
 
 func getAdjacentSymbol(row int, start_col int, end_col int, matrix [][]rune) ([]int, error){
   search_area := [][]int{{row, start_col-1}, {row, end_col}}
-
-  for i := row-1; i < row; i++ {
-    for j := start_col-1; j < end_col; j++ {
+  for i := row-1; i < row+2; i++ {
+    for j := start_col-1; j < end_col+1; j++ {
       search_area = append(search_area, []int{i, j})
     }
   }
+  fmt.Println(search_area)
 
   for _, coordinate := range search_area {
     r := coordinate[0]
     c := coordinate[1]
 
-    if r < 0 || r >= len(matrix) || c < 0 || c >= len(matrix[0]) {
+    if r < 0 || r >= len(matrix) || c < 0 || c >= len(matrix[r]) {
       continue
     }
 
     candidate := matrix[r][c]
-    if unicode.IsDigit(candidate) && candidate != '.' {
+    if ! unicode.IsDigit(candidate) && candidate != '.' {
       return []int{r, c}, nil
     }
   }
@@ -76,14 +76,16 @@ func main() {
     for c, col := range row {
       if unicode.IsDigit(col){
         num *= 10
-        num += int(col)
+        prsd, _ := strconv.ParseInt(string(col), 10, 0)
+        num += int(prsd)
         continue
       }
       if num == 0 {
         continue
       }
-      col_start := c - len(string(num))
-      _, err := getAdjacentSymbol(r, col_start, c, matrix); if err != nil {
+      col_start := c - len(fmt.Sprintf("%d", num))
+      fmt.Println(num, col_start, len(fmt.Sprintf("%d", num)))
+      _, err := getAdjacentSymbol(r, col_start, c, matrix); if err == nil {
         partNum = append(partNum, num)
       }
       num = 0
