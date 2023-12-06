e79d84754d9f5b630e9e584a1d986eac3b0311a9
JP <dragonginger10@gmail.com>
2023-12-05 20:14:54 -0600

    setup for day 5

--- day_5/main.go
+++ day_5/main.go
@@ -1,21 +1,85 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
+type section struct {
+  sectionName string
+  sectionInts [][]int
+}
+
+type mapping struct {
+  destinationRange int
+  sourceRange int
+  rangeLength int
+}
+
+func parseIntList(str string) []int {
+  var result []int
+  seperated := strings.Split(str, " ")
+  for _, s := range seperated {
+    i, _ := strconv.ParseInt(s, 10, 0)
+    result = append(result, int(i))
+  }
+  return result
+}
+
 func ReadInput(file string) []string {
-	rawFile, err := os.ReadFile(file)
+  var result []string
+	rawFile, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return strings.Split(string(rawFile), "\n")
-}
-func main() {
-	cards := ReadInput(os.Args[1])
+  defer rawFile.Close()
 
-	fmt.Println(cards)
+  scanner := bufio.NewScanner(rawFile)
+  for scanner.Scan() {
+    readLine := scanner.Text()
+    result = append(result, readLine)
+  }
+  return result
+}
+
+func getSection(almanacPart []string) []section {
+  var result []section
+  re := regexp.MustCompile(` map:`)
+  var newSection section
+  for _, line := range almanacPart {
+    if re.Match([]byte(line)) {
+      name := re.ReplaceAll([]byte(line), []byte{})
+      newSection.sectionName = string(name)
+      continue
+    }
+    if line == ""{
+      result = append(result, newSection)
+      newSection.sectionInts = make([][]int, 0)
+      continue
+    }
+    parsed := parseIntList(line)
+    newSection.sectionInts = append(newSection.sectionInts, parsed)
+  }
+  return result
+}
+
+func main() {
+	almanac := ReadInput(os.Args[1])
+
+  seeds := strings.Split(almanac[0], ": ")
+  seeds = strings.Split(seeds[1], " ")
+
+  sections := getSection(almanac[2:])
+
+  for _, section := range sections {
+    fmt.Println(section.sectionName)
+    fmt.Println(section.sectionInts)
+    fmt.Println("********************************************************************************")
+  }
+
 }
