02d66228405b36959de7bae839b10c52191df15e
JP <dragonginger10@gmail.com>
2023-12-07 15:42:51 -0600

    solved part one, working through part two

--- day_7/main.go
+++ day_7/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type handInfo struct {
     hand string
+    handNum []int
     handValue int
     bid int
     ranking int
@@ -33,7 +35,19 @@ func parseHandValue(hand string) int {
         counts[card]++
 	}
 
-	for _, count := range counts {
+    num := 0
+    for _, count := range counts {
+        if count > num {
+            num = count
+        }
+    }
+
+	for card, count := range counts {
+        if count == num && card != 'J'{
+            count += counts['J']
+            num = 0
+        }
+
 		switch count {
 		case 5:
 			handValue = 6
@@ -52,12 +66,12 @@ func parseHandValue(hand string) int {
 	return handValue
 }
 
-func rankHand(h *handInfo, list []handInfo) []handInfo {
+func cardToInt(card rune) int {
 	cardValue := map[rune]int{
 		'A': 14,
 		'K': 13,
 		'Q': 12,
-		'J': 11,
+		'J': 1,
 		'T': 10,
 		'9': 9,
 		'8': 8,
@@ -69,36 +83,16 @@ func rankHand(h *handInfo, list []handInfo) []handInfo {
 		'2': 2,
 	}
 
-    for i, hand := range list {
-        if hand.handValue == h.handValue {
-            for i, c := range h.hand {
-                valOne := cardValue[c]
-                valTwo := cardValue[rune(hand.hand[i])]
-                if valOne == valTwo {
-                    continue
-                } else if valOne > valTwo && h.ranking < hand.ranking{
-                    h.ranking, hand.ranking = hand.ranking, h.ranking
-                    break
-                } else if valOne < valTwo && h.ranking > hand.ranking {
-                    h.ranking, hand.ranking = hand.ranking, h.ranking
-                    break
-                }
-            }
-            continue
-        }
+    return cardValue[card]    
+}
 
-        if h.handValue > hand.handValue && h.ranking < hand.ranking {
-            h.ranking, hand.ranking = hand.ranking, h.ranking
-        } else if h.handValue < hand.handValue && h.ranking > hand.ranking{
-            h.ranking, hand.ranking = hand.ranking, h.ranking
-        }
+func handToInt(hand string) []int {
+    var list []int
 
-        if h.ranking == hand.ranking && h.handValue > hand.handValue{
-            hand.ranking--
-        }
-
-        list[i] = hand
+    for _, card := range hand {
+        list = append(list, cardToInt(card))
     }
+
     return list
 }
 
@@ -108,9 +102,6 @@ func main() {
     var handRanking []handInfo 
 	listOfHands := ReadInput(os.Args[1])
 
-    answer += bid
-
-
 	for i, line := range listOfHands {
         i++
         if line == "" {
@@ -118,22 +109,34 @@ func main() {
         }
 		handBid := strings.Split(line, " ")
 		hand := handBid[0]
-        value := parseHandValue(hand)
 		bid, _ = strconv.ParseInt(handBid[1], 10, 64)
 
         handRanking = append(handRanking, handInfo{
             hand: hand,
-            handValue: value,
+            handNum: handToInt(hand),
+            handValue: parseHandValue(hand),
             bid: int(bid),
-            ranking: i,
         })
 	}
 
-    copyRankings := handRanking
-    for _, h := range handRanking {
-        copyRankings = rankHand(&h, copyRankings)
+    sort.Slice(handRanking, func(i, j int) bool {
+        if handRanking[i].handValue == handRanking[j].handValue {
+            for x := 0; x < len(handRanking[i].handNum); x++ {
+                if handRanking[i].handNum[x] != handRanking[j].handNum[x]{
+                    return handRanking[i].handNum[x] < handRanking[j].handNum[x]
+                }
+            }
+        }
+        return handRanking[i].handValue < handRanking[j].handValue
+    })
+
+
+    for rank, h := range handRanking {
+        rank++
+        // fmt.Printf("Hand: %s\nBid: %d\nHand Value %d\nRank: %d\n", h.hand, h.bid, h.handValue, rank)
+        answer += int64(rank) * int64(h.bid)
     }
-    
-    fmt.Println(handRanking)
+
+        
 	fmt.Println(answer)
 }
