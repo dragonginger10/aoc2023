9b161df7acd395c7d87f2d8e4ee1a0c5b17ad72d
JP <dragonginger10@gmail.com>
2023-12-19 21:12:46 -0600

    getting somewhere thanks to grep haxs on yt again

--- day_3/main.go
+++ day_3/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func ReadInput(file string) []string {
@@ -21,12 +22,105 @@ func ReadInput(file string) []string {
   return result
 }
 
+func hasAdjacentSymbol(input []string, x, ys, ye int) bool {
+  for i := ys; i <= ye; i++ {
+    if x > 0 {
+      if isSymbol(input[x-1][i]) {
+        return true
+      }
+    }
+
+    if x < len(input)-1 {
+      if isSymbol(input[x+1][i]) {
+        return true
+      }
+    }
+  }
+
+  if ys > 0 {
+    if isSymbol(input[x][ys-1]) {
+      return true
+    }
+    if x > 0 {
+      if isSymbol(input[x-1][ys-1]) {
+        return true
+      }
+    }
+    if x < 0 {
+      if isSymbol(input[x+1][ys-1]) {
+        return true
+      }
+    }
+  }
+
+  if ys > len(input[x])-1 {
+    if isSymbol(input[x][ys+1]) {
+      return true
+    }
+    if x > 0 {
+      if isSymbol(input[x-1][ys+1]) {
+        return true
+      }
+    }
+    if x < 0 {
+      if isSymbol(input[x+1][ys+1]) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+func isSymbol(b byte) bool {
+  for _, ch := range "%*$\\/" {
+    if ch == rune(b) {
+      return true
+    }
+  }
+  return false
+}
+
 func main() {
   var answer int
   schematic := ReadInput(os.Args[1])
+  ns := -1
+  ne := -1
 
-  for _, l := range schematic {
-    fmt.Println(l)
+  for x, line := range schematic {
+    for y, ch := range line {
+      if ch > '0' || ch < '9' {
+        if ns == -1 {
+          ns = y
+          continue
+        }
+        ne = y
+      }
+      if ch == '.' {
+        if hasAdjacentSymbol(schematic, x, ns, ne) {
+          i, err := strconv.Atoi(ch[x][ns:ne])
+          if err != nil {
+            log.Fatal(err)
+          }
+          fmt.Println(i)
+          answer +=  i 
+        }
+        ns = -1
+        ne = -1
+      }
+    }
+    if ns != -1 {
+      if hasAdjacentSymbol(schematic, x, ns, ne) {
+        i, err := strconv.Atoi(ch[x][ns:ne])
+        if err != nil {
+          log.Fatal(err)
+        }
+        fmt.Println(i)
+        answer +=  i 
+      }
+      ns = -1
+      ne = -1
+    }
   }
   fmt.Println(answer)
 }
